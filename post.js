821f111c81447423ceff9f28fb78b24305f9d35a
MatiasCS1997 <mcorderos@miucsh.cl>
2023-01-02 02:33:48 -0300

    app put, app delete

--- post.js
+++ post.js
@@ -7,7 +7,33 @@ const connectionData = {
   password: "44182",
   port: 5433,
 };
-
 const clientDB = new Client(connectionData);
 
-module.exports = clientDB;
+const addPost = async (titulo, url, descripcion) => {
+  const query = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3, 0)";
+  const values = [titulo, url, descripcion];
+  const result = await pool.query(query, values);
+  console.log(" Tu post ha sido agregado");
+};
+
+const getPosts = async () => {
+  const { rows } = await pool.query("SELECT * FROM posts");
+  console.log(rows);
+  return rows;
+};
+
+const addLike = async (id) => {
+  const query = "UPDATE posts SET likes = likes + 1 WHERE id = $1";
+  const values = [id];
+  const result = await pool.query(query, values);
+};
+
+const deletePost = async (id) => {
+  const query = "DELETE FROM posts WHERE id = $1";
+  const values = [id];
+  const result = await pool.query(query, values);
+};
+
+module.exports = { addPost, getPosts, addLike, deletePost };
+
+
--- server.js
+++ server.js
@@ -19,3 +19,33 @@ app.get("/posts", async (req, res) => {
   const posts = await getPosts();
   res.json(posts);
 });
+
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, url, descripcion } = req.body;
+    await addPost(titulo, url, descripcion);
+    res.send("Su post ha sido agregado con éxito");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.put("/posts/like/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await addLike(id);
+    res.send("Su like ha sido agregado con éxito");
+  } catch (error) {
+    res.status(500).send("Error, su like no pudo ser agregado.");
+  }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  await deletePost(id);
+  res.send("Post eliminado con éxito");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
